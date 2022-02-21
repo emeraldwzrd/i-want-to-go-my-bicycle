cfc4c1b349233eefaaf827cb2263a8d8160995b7
Michael Rodriguez <iamrod@users.noreply.github.com>
2022-02-20 22:01:34 -0500

    Made some changes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamrod/i-want-to-go-my-bicycle
+
+go 1.17
--- main.go
+++ main.go
@@ -1,52 +1,58 @@
 package main
 
+import "fmt"
+
 type bicycle struct {
-	size  int
-	parts parts
+	size  string
+	parts *parts
 }
 
-func (b *bicycle) initialize(size int, parts parts) {
-	b.size = size
-	b.parts = parts
-}
-
-func (b *bicycle) spares(size int, parts parts) {
-	b.size = size
-	b.parts = parts
-}
-
-type parts struct {
-	spares int
-	size   int
-	parts  int
-}
-
-func (p *parts) initialize(parts int) {
-	p.parts = parts
-}
-
-//func (p *parts) spares(parts int) {
-//	p.parts = parts
-//}
-
-type partsfactory interface {
-	//name        string
-	//description string
-	//needsSpare  string
-}
-
-func build(config []int) parts {
-	return parts{
-		spares: 0,
-		size:   0,
-		parts:  0,
+func NewBicycle(size string, config [][]string) *bicycle {
+	return &bicycle{
+		size:  size,
+		parts: build(config),
 	}
 }
 
-func createPart(partConfig []string) map[string]string {
-	pf := make(map[string]string)
-	pf["name"] = partConfig[0]
-	pf["description"] = partConfig[1]
-	pf["needsSpare"] = partConfig[2]
-	return pf
+type parts []partItem
+type partItem struct {
+	name        string
+	description string
+	needsSpare  bool
+}
+
+type PartsMaker interface {
+	build(config map[string]string, parts *parts) parts
+}
+
+func build(config [][]string) *parts {
+	var parts parts
+	for i := range config {
+		parts = append(parts, createPart(config[i]))
+	}
+	return &parts
+}
+
+func createPart(partConfig []string) partItem {
+	needsSpare := true
+	if len(partConfig) > 2 && partConfig[2] == "false" {
+		needsSpare = false
+	}
+
+	return partItem{
+		name:        partConfig[0],
+		description: partConfig[1],
+		needsSpare:  needsSpare,
+	}
+}
+
+func main() {
+	roadConfig := [][]string{
+		{"chain", "11-speed"},
+		{"tireSize", "23"},
+		{"tapeColor", "red"},
+	}
+	roadBike := NewBicycle("L", roadConfig)
+
+	fmt.Printf("%v\n", roadBike.parts)
 }
